22d3ad40083c0e3a6da11fa41948353323e94067
Rudolf Traunmüller <rudolf.traunmueller@maat.at>
2017-12-02 08:36:48 +0100

    parsing tournaments and players

--- src/main/java/at/swingolf/appinit/HelloController.java
+++ src/main/java/at/swingolf/appinit/HelloController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.*;
 import java.util.Collection;
+import java.util.LinkedList;
 
 @RestController
 public class HelloController {
@@ -26,12 +27,18 @@ public class HelloController {
     @RequestMapping("/import")
     public String importer() throws IOException {
         Collection<File> files = FileUtils.listFiles(new File(fileDirectory), new String[]{"xlsx"}, false);
-        files.stream().forEach(file -> parseFile(file));
+        files.stream().map(file -> parseFile(file)).flatMap(tournament -> tournament.stream()).forEach(tournament -> {
+            printTournament(tournament);});
         return "Import succeeded!";
     }
 
-    private void parseFile(File file) {
+    private void printTournament(Tournament tournament) {
+        System.out.println(tournament.toString());
+        tournament.getPlayers().stream().forEach(player -> System.out.println(player));
+    }
 
+    private Collection<Tournament> parseFile(File file) {
+        Collection<Tournament> tournaments = new LinkedList<>();
         try (
 
                 InputStream is = new FileInputStream(file);
@@ -40,10 +47,54 @@ public class HelloController {
                         .bufferSize(4096)
                         .open(is)) {
             for (Sheet sheet : workbook) {
-                System.out.println(sheet.getSheetName());
+                Tournament tournament = new Tournament();
+                tournaments.add(tournament);
+                String sheetName = sheet.getSheetName();
+                tournament.setSheetName(sheetName);
+
+
                 for (Row r : sheet) {
+                    boolean validRow = false;
+                    Player player = new Player();
                     for (Cell c : r) {
-                        System.out.println(c.getStringCellValue());
+                        // place
+                        if (matches(r, c,0,1)) {
+                            tournament.setPlace(c.getStringCellValue());
+                        }
+                        // date
+                        if (matches(r, c,1,1)) {
+                            tournament.setDate(c.getStringCellValue());
+                        }
+                        // name
+                        if (matches(r, c,2,1)) {
+                            tournament.setName(c.getStringCellValue());
+                        }
+                        // referee
+                        if (matches(r, c,3,1)) {
+                            tournament.setReferee(c.getStringCellValue());
+                        }
+                        // best15
+                        if (matches(r, c,4,1)) {
+                            tournament.setBest15(c.getStringCellValue());
+                        }
+                        // best20
+                        if (matches(r, c,5,1)) {
+                            tournament.setBest20(c.getStringCellValue());
+                        }
+                        if (r.getRowNum()>=7) {
+                            if (c.getColumnIndex()==0 && c.getStringCellValue().length()>2) {
+                                    validRow = true;
+                            }
+                            if (validRow && c.getColumnIndex() == 0) {
+                                tournament.getPlayers().add(player);
+                                player.setName(c.getStringCellValue());
+                            }
+                            if (validRow && c.getColumnIndex()>=3 && c.getColumnIndex()<=3+18) {
+
+                            }
+                        }
+
+//                        System.out.println(c.getStringCellValue());
                     }
                 }
             }
@@ -52,6 +103,12 @@ public class HelloController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return tournaments;
     }
+
+    private boolean matches(Row r, Cell c, int rowNum, int colNum) {
+        return r.getRowNum() == rowNum && c.getColumnIndex() == colNum;
+    }
+
 
 }
---
+++ src/main/java/at/swingolf/appinit/Player.java
@@ -0,0 +1,23 @@
+package at.swingolf.appinit;
+
+/**
+ * Created by rudolf.traunmueller on 02.12.2017.
+ */
+public class Player {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
